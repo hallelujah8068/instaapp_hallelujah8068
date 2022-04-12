8c01cbdc8e00484b8d447480ffde30af74c4c000
松井晴也 <>
2022-04-12 21:36:55 +0900

    ajaxでいいね機能を実装

---
+++ app/javascript/packs/article.js
@@ -0,0 +1,25 @@
+import $ from 'jquery'
+import axios from 'modules/axios'
+import {
+  handleHeartDisplay,
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+} from 'modules/handle_heart'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.active-heart').each(function() {
+    const articleId = $(this).attr('id')
+    //いいね機能
+    axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      const hasLiked = response.data.hasLiked
+      handleHeartDisplay(hasLiked, articleId)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  
+    listenInactiveHeartEvent(articleId)
+    listenActiveHeartEvent(articleId)
+    })
+})
