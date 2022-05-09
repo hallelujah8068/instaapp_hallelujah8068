e3d120c1bbc164b2395f5155b353e0db455567ca
松井晴也 <>
2022-05-10 01:21:57 +0900

    ajaxでのフォロー表示のバグを修正

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,11 +2,10 @@ class AccountsController < ApplicationController
     def show
         @user = User.find(params[:id])
         follow_status = current_user.has_followed?(@user)
-        follower = @user.followers.count
 
         respond_to do |format|
             format.html
-            format.json { render json: { hasFollowed: follow_status, follower: follower } }
+            format.json { render json: { hasFollowed: follow_status } }
         end
         
         if @user == current_user
--- app/javascript/packs/follows.js
+++ app/javascript/packs/follows.js
@@ -1,5 +1,9 @@
 import $ from 'jquery'
-import axios from 'modules/axios'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
 
 
 const handleFollowDisplay = (hasFollowed) => {
@@ -8,49 +12,42 @@ const handleFollowDisplay = (hasFollowed) => {
     } else {
       $('.follow').removeClass('hidden')
     }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
     const dataset = $('#accounts-show').data()
     const accountId = dataset.id
-
     axios.get(`/accounts/${accountId}.json`)
       .then((response) => {
         const hasFollowed = response.data.hasFollowed
-        const follower = response.data.follower
-        $('.follower_count').append(`<span>${follower}</span>`)
         handleFollowDisplay(hasFollowed)
+      })
+  
+    $('.follow').on('click', () => {
+      axios.post(`/accounts/${accountId}/follows`)
+        .then((response) => {
+          if (response.data.status === 'ok'){
+            $('.unfollow').removeClass('hidden')
+            $('.follow').addClass('hidden')
+          }
+        })
+        .catch((e) => {
+          window.alert('error')
+          console.log(e)
+        })
     })
-}) 
-
-$('.follow').on('click', () => {
-    axios.post(`/accounts/${accountId}/follows`)
-      .then((response) => {
-        const follower = response.data.follower
-        if (response.data.status === 'ok'){
-          $('.unfollow').removeClass('hidden')
-          $('.follow').addClass('hidden')
-          $('.follower_count').text(follower)
-        }
-      })
-      .catch((e) => {
-        window.alert('error')
-        console.log(e)
-      })
-  })
-
-  $('.unfollow').on('click', () => {
-    axios.post(`/accounts/${accountId}/unfollows`)
-      .then((response) => {
-        const follower = response.data.follower
-        if (response.data.status === 'ok'){
-          $('.follow').removeClass('hidden')
-          $('.unfollow').addClass('hidden')
-          $('.follower_count').text(follower)
-        }
-      })
-      .catch((e) => {
-        window.alert('error')
-        console.log(e)
-      })
+  
+    $('.unfollow').on('click', () => {
+      axios.post(`/accounts/${accountId}/unfollows`)
+        .then((response) => {
+          if (response.data.status === 'ok'){
+            $('.follow').removeClass('hidden')
+            $('.unfollow').addClass('hidden')
+          }
+        })
+        .catch((e) => {
+          window.alert('error')
+          console.log(e)
+        })
+    })
   })
