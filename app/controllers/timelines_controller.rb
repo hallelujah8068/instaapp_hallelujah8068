d12808c099ac45e214626ea693ad08b2cfac8a2a
松井晴也 <>
2022-05-12 22:48:09 +0900

    フォロー中のユーザーの投稿を新着順に表示するように変更

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,8 +3,8 @@ class TimelinesController < ApplicationController
 
     def show
         user_ids = current_user.followings.pluck(:id)
-        from = Time.now - 24.hours
-        to = Time.now
-        @articles = Article.where(user_id: [*user_ids, current_user.id], created_at: from..to).left_joins(:likes).group(:id).order('COUNT(likes.id) DESC',created_at: :desc).limit(5)
+        # from = Time.now - 24.hours
+        # to = Time.now
+        @articles = Article.where(user_id: user_ids).order(created_at: :desc)
     end
 end
